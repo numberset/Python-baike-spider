271ca309d4ff013cd9d655ecfbb9d6936ded9ce2
numberset <zxz499@case.edu>
2019-06-02 21:34:28 -0400

    20190602

--- html_downloader.py
+++ html_downloader.py
@@ -1,11 +1,21 @@
 # 百度百科Python爬虫实例
+# https://www.imooc.com/learn/563
 # https://github.com/lovefengruoqing/baike_spider
 
+import urllib.request
 
 class HtmlDownloader(object):
 
 
-    def download(self):
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = urllib.request.urlopen(url)
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
 
 
     
